a2c2d48d1a907d4e751409557706cb58b35af206
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-06 16:06:18 +0500

    card details

---
+++ utils/CourseCardInterface.ts
@@ -0,0 +1,19 @@
+export interface CourseCardProps {
+  course: {
+    courseTitle: string
+    universityName: string
+    universityLocation: string
+    country: string
+    campusCity: string
+    courseTypeWithDuration: string
+    tuitionFeeRefined: string
+    duration: string
+    Summary: string
+    minimumEducationLevel: string
+    minimumGPA: string
+    ielts: string
+    toefl: string
+    pte: string
+    duolingo: string
+  }
+}
