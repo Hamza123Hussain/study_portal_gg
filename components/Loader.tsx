baeaf6f278f4af1a11efade6d0a2df472461a23a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-06 18:09:28 +0500

    loader has been added

---
+++ components/Loader.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className=" flex justify-center items-center min-h-screen">
+      <div className="banter-loader">
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+        <div className="banter-loader__box"></div>
+      </div>
+    </div>
+  )
+}
+
+export default Loader
