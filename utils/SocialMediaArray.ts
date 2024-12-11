7cc3b11ef1831335a68d11e6481eb4cb4d6da0bc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-11 14:01:43 +0500

    array for study portal has been created

---
+++ utils/SocialMediaArray.ts
@@ -0,0 +1,19 @@
+import { FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { FcGoogle } from 'react-icons/fc'
+export const socialMediaData = [
+  {
+    icon: FcGoogle,
+    backgroundColor: 'white',
+    url: 'https://globalgrads.org/',
+  },
+  {
+    icon: FaInstagram,
+    backgroundColor: '#ac2bac',
+    url: 'https://www.instagram.com/globalgrads_org?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==',
+  },
+  {
+    icon: FaLinkedin,
+    backgroundColor: '#0082ca',
+    url: 'https://www.linkedin.com/company/99827923/admin/dashboard/',
+  },
+]
