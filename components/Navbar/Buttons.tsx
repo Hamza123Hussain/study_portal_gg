492db44bdb1bdf82f14a03ec37422c4225a65805
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-16 14:47:31 +0500

    Update Buttons.tsx

--- components/Navbar/Buttons.tsx
+++ components/Navbar/Buttons.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Buttons = () => {
   return (
     <>
-      <div className="relative">
+      {/* <div className="relative">
         <input
           type="text"
           placeholder="Search..."
@@ -12,7 +12,7 @@ const Buttons = () => {
         <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
           🔍
         </span>
-      </div>
+      </div> */}
       <button className="bg-green-600 py-2 px-4 rounded-full text-white hover:bg-green-700 transition-all duration-200 w-full">
         Book Consultation
       </button>
