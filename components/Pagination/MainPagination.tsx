b41abb491e4b08af194831474251259c4094da0c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-12-16 14:47:12 +0500

    Create MainPagination.tsx

---
+++ components/Pagination/MainPagination.tsx
@@ -0,0 +1,13 @@
+import { Pagination } from '@nextui-org/react'
+
+export default function MainPagination({ TotalPages }: { TotalPages: number }) {
+  return (
+    <div className=" flex items-center p-3  justify-center  ">
+      <Pagination
+        className=" border-2 border-black rounded-xl bg-gray-100"
+        initialPage={1}
+        total={TotalPages}
+      />
+    </div>
+  )
+}
