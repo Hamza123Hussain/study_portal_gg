be335470c55c8dd7c1211cc09277c568acac0238
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-20 16:58:16 +0500

    Create CartImage.tsx

---
+++ components/Cart/CartImage.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import React from 'react'
+
+const CartImage = () => {
+  return (
+    <Image
+      src={'/182cd6871cb8aa1eac5e820abd9a56c0-Photoroom.png'}
+      alt="Cart Logo"
+      width={50}
+      height={50}
+    />
+  )
+}
+
+export default CartImage
