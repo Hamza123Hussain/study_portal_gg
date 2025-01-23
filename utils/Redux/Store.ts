b4c02c144e03927ad21a15955ea247839e193a39
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-23 14:25:46 +0500

    Update Store.ts

--- utils/Redux/Store.ts
+++ utils/Redux/Store.ts
@@ -1,24 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import { CartSlice } from './Slice/CartSlice'
 
+// Combine all reducers into one root reducer
+const rootReducer = combineReducers({
+  Cart: CartSlice.reducer,
+  // Add additional reducers here
+  // user: UserSlice.reducer,
+  // location: Location_Slice.reducer,
+  // sort: SortSlice.reducer,
+})
+
+// Configure the store
 const Store = configureStore({
-  reducer: {
-    Cart: CartSlice.reducer,
-    // user: UserSlice.reduQcer, // Add UserSlice reducer here
-    // Select: SelectSlice.reducer,
-    // location: Location_Slice.reducer,
-    // sort: SortSlice.reducer,
-    // Flag: Flag_Slice.reducer,
-    // Chat: ChatSlice.reducer,
-    // Kpi: UserKpiSlice.reducer,
-    // KpiList: KpiListSlice.reducer,
-    // UserSelect: userSlice.reducer,
-    // userTaskSlice: UserTasksSlice.reducer,
-  },
+  reducer: rootReducer, // Pass the combined reducers
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false, // Disable serializability checks if needed
+    }),
+  devTools: true, // Enable Redux DevTools by default
 })
 
 export default Store
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof Store.getState>
-// Inferred type: {some: SomeStateType}
+
+// Infer the `RootState` and `AppDispatch` types
+export type RootState = ReturnType<typeof rootReducer>
 export type AppDispatch = typeof Store.dispatch
