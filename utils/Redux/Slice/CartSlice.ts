ca7b0123efa9cb4d90347bc2288541c82d577037
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-23 14:52:14 +0500

    FIXED MINOR CHANGE

--- utils/Redux/Slice/CartSlice.ts
+++ utils/Redux/Slice/CartSlice.ts
@@ -1,30 +1,31 @@
-import { Cart } from '@/utils/CartInterface'
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
 import toast from 'react-hot-toast'
-const initialState: Cart[] = []
+import { Cart, Course } from '@/utils/CartInterface'
+
+const initialState: Cart = {
+  Course: [],
+  totalOptions: 0,
+}
+
 export const CartSlice = createSlice({
   name: 'Cart',
   initialState,
   reducers: {
-    SetCartData: (state, action) => {
-      state = action.payload
+    SetCartData: (state, action: PayloadAction<Cart>) => {
+      return action.payload // Return new state with updated cart data
     },
-    AddToCart: (state, action) => {
-      const DuplicateCourse = state.filter((cart) =>
-        cart.Course.filter(
-          (course) => course.CourseID === action.payload.CourseID
-        )
-      )
-      if (DuplicateCourse.length > 0) toast.error('Course already in cart') // Show toast message
-      state.push(action.payload)
+    AddToCart: (state, action: PayloadAction<Course>) => {
+      state.Course.push(action.payload) // Add course to cart
+      state.totalOptions += 1
     },
-    RemoveFromCart: (state, action) => {
-      state = state.filter((cart) =>
-        cart.Course.filter(
-          (course) => course.CourseID !== action.payload.CourseID
-        )
+    RemoveFromCart: (state, action: PayloadAction<string>) => {
+      state.Course = state.Course.filter(
+        (course) => course.CourseID !== action.payload
       )
+      state.totalOptions -= 1
     },
   },
 })
+
 export const { SetCartData, AddToCart, RemoveFromCart } = CartSlice.actions
+export default CartSlice.reducer
