fe6a0991a65f5d63c2047ccb6c241b49b56c8526
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-01-23 14:25:58 +0500

    not adding duplicates to cart

--- utils/Redux/Slice/CartSlice.ts
+++ utils/Redux/Slice/CartSlice.ts
@@ -1,5 +1,6 @@
 import { Cart } from '@/utils/CartInterface'
 import { createSlice } from '@reduxjs/toolkit'
+import toast from 'react-hot-toast'
 const initialState: Cart[] = []
 export const CartSlice = createSlice({
   name: 'Cart',
@@ -9,6 +10,12 @@ export const CartSlice = createSlice({
       state = action.payload
     },
     AddToCart: (state, action) => {
+      const DuplicateCourse = state.filter((cart) =>
+        cart.Course.filter(
+          (course) => course.CourseID === action.payload.CourseID
+        )
+      )
+      if (DuplicateCourse.length > 0) toast.error('Course already in cart') // Show toast message
       state.push(action.payload)
     },
     RemoveFromCart: (state, action) => {
