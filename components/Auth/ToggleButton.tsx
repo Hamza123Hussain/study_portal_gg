2267e5298f55e246c6793694518a9413ecc06a43
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 14:22:02 +0500

    Create ToggleButton.tsx

---
+++ components/Auth/ToggleButton.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const ToggleButton = ({
+  isLogin,
+  setIsLogin,
+}: {
+  isLogin: boolean
+  setIsLogin: (auth: boolean) => void
+}) => {
+  return (
+    <div className="flex gap-8 mb-4">
+      <button
+        onClick={() => setIsLogin(true)}
+        className={`px-4 py-2 rounded-lg ${
+          isLogin ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        }`}
+      >
+        Login
+      </button>
+      <button
+        onClick={() => setIsLogin(false)}
+        className={`px-4 py-2 rounded-lg ${
+          !isLogin ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        }`}
+      >
+        Sign Up
+      </button>
+    </div>
+  )
+}
+
+export default ToggleButton
