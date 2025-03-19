18bc75c902eb66a4152553f11cacf871eb6bad48
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 15:41:09 +0500

    Create NewStudentAdd.ts

---
+++ functions/Students/NewStudentAdd.ts
@@ -0,0 +1,24 @@
+import { FormData } from '@/utils/Main_Form_Interface'
+import { APIURL } from '@/utils/SignupInterface'
+import axios from 'axios'
+
+const createStudent = async (studentData: FormData) => {
+  try {
+    const response = await axios.post(
+      `${APIURL}Api/Student/NewStudent`,
+      studentData
+    )
+    if (response.status === 201) {
+      // console.log('Student created successfully:', response.data)
+      return response.data
+    }
+  } catch (error: any) {
+    console.error('Error creating student:', error.response?.data || error)
+    if (error.response?.status === 400) {
+      throw new Error('Duplicate entry. Student already exists.')
+    }
+    throw new Error('An unexpected error occurred.')
+  }
+}
+
+export default createStudent
