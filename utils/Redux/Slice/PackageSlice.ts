ff65bb33ced6f093d8b62504d3e6c4eccc2ed254
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 13:54:11 +0500

    Create PackageSlice.ts

---
+++ utils/Redux/Slice/PackageSlice.ts
@@ -0,0 +1,28 @@
+import { Package } from '@/utils/Interface/PackageInterface'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+const initialState: Package = {
+  PackageCountry: '',
+  PackagePrice: '',
+  PackageType: '',
+  NumberOFUniversities: '',
+  Applicationfeeincluded: false,
+  VisaAssistance: false,
+  SOP_LORAssistance: false,
+  IELTSAssistance: false,
+  UniversityShortlisting: false,
+  ScholarshipAssistance: false,
+  PostVisaServices: false,
+  UniversityApplicationAssistance: false,
+  VisaInterviewPreparation: false,
+}
+export const PackageSlice = createSlice({
+  name: 'Package',
+  initialState,
+  reducers: {
+    SetPackageData: (state, action: PayloadAction<any>) => {
+      return action.payload // Replace state with the new cart data
+    },
+  },
+})
+export const { SetPackageData } = PackageSlice.actions
+export default PackageSlice.reducer
