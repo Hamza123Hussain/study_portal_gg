8df33aa7500beb178cf3005a871dd4494cba082c
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 15:41:43 +0500

    Create Step5.tsx

---
+++ components/Form/Steps/Step5.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import './Step5.module.css'
+import { RootState } from '@/utils/Redux/Store'
+const Step5: React.FC = () => {
+  const formData = useSelector((state: RootState) => state.PersonalDetails)
+  const flag = useSelector((state: RootState) => state.Flag)
+  return (
+    <div className="space-y-4">
+      {flag ? (
+        <div className="flex-container flex justify-center items-center min-h-screen">
+          {/* Spinner loader with class-based styling */}
+          <div className="spinner"></div>
+          <span>Form is being submitted...</span>
+        </div>
+      ) : (
+        <>
+          <h3 className="font-bold">Summary of Your Data</h3>
+          {Object.entries(formData).map(([key, value]) => (
+            <div key={key}>
+              <strong>{key.replace(/([A-Z])/g, ' $1').toUpperCase()}:</strong>{' '}
+              {value || 'Not Provided'}
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  )
+}
+export default Step5
