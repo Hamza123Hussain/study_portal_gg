cbfa6fa4a058589db13e467b0e3176a22f19e4fc
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 13:54:41 +0500

    Create PackageLoader.tsx

---
+++ components/Package/PackageLoader.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+const PackageLoader = () => {
+  return (
+    <div className="wrapper">
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="circle"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+      <div className="shadow"></div>
+    </div>
+  )
+}
+export default PackageLoader
