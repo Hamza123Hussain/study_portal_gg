95e45ddcfd869d7415f5db3ed7599d303f057d95
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 13:54:02 +0500

    Create PackageInterface.ts

---
+++ utils/Interface/PackageInterface.ts
@@ -0,0 +1,16 @@
+export interface Package {
+  _id: string
+  PackageCountry: string
+  PackagePrice: string
+  PackageType: string
+  NumberOFUniversities: string
+  Applicationfeeincluded: boolean
+  VisaAssistance: boolean
+  SOP_LORAssistance: boolean
+  IELTSAssistance: boolean
+  UniversityShortlisting: boolean
+  ScholarshipAssistance: boolean
+  PostVisaServices: boolean
+  UniversityApplicationAssistance: boolean
+  VisaInterviewPreparation: boolean
+}
