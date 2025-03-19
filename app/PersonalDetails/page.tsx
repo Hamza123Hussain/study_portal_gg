4c7feac52fe9d86098136a33a0a2e776cdbd030e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-19 15:40:52 +0500

    Create page.tsx

---
+++ app/PersonalDetails/page.tsx
@@ -0,0 +1,12 @@
+import MovingBetweenSteps from '@/components/Form/MovingBetweenSteps'
+import React from 'react'
+
+const PersonalDetails = () => {
+  return (
+    <div className=" flex flex-col">
+      <MovingBetweenSteps />
+    </div>
+  )
+}
+
+export default PersonalDetails
