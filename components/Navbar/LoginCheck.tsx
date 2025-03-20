95e7855f821b48dc1a58e10f75959744af05f4ad
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-20 12:54:33 +0500

    Update LoginCheck.tsx

--- components/Navbar/LoginCheck.tsx
+++ components/Navbar/LoginCheck.tsx
@@ -4,8 +4,12 @@ import { RootState } from '@/utils/Redux/Store'
 import AuthModal from '../Auth/AuthModal'
 import MainCart from '../Cart/MainCart'
 import { UserModal } from '../Auth/UserModal'
+import { usePathname, useRouter } from 'next/navigation'
+import { Home } from 'lucide-react'
 const LoginCheck = () => {
+  const Router = useRouter()
   const UserName = useSelector((state: RootState) => state.user.Name)
+  const Pathname = usePathname()
   return (
     <>
       {UserName !== '' ? (
@@ -13,6 +17,14 @@ const LoginCheck = () => {
           <MainCart />
           <UserModal />
         </div>
+      ) : Pathname === '/PersonalDetails' ? (
+        <button
+          onClick={() => Router.push('/')}
+          className="flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-full shadow-md hover:bg-blue-700 transition"
+        >
+          <Home size={20} />
+          Go To Home
+        </button>
       ) : (
         <AuthModal />
       )}
