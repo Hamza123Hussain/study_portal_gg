0c8aa408f6c7152303dbe216243b86dcd2da450e
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-21 15:41:38 +0500

    Update CourseList.tsx

--- components/Order/Course/CourseList.tsx
+++ components/Order/Course/CourseList.tsx
@@ -1,8 +1,9 @@
 import { BookOpen } from 'lucide-react'
 import React from 'react'
 import CourseCard from './CourseCard'
-import { Course } from '@/utils/Interface/OrderInterface'
-const CourseList = ({ Course }: { Course: Course[] }) => {
+import { CourseID } from '@/utils/Interface/OrderInterface'
+
+const CourseList = ({ Course }: { Course: CourseID[] }) => {
   return (
     <div className="bg-white rounded-2xl shadow-xl p-6 sm:p-8">
       <h2 className="text-3xl font-bold text-blue-800 mb-6 flex items-center gap-2">
@@ -10,7 +11,7 @@ const CourseList = ({ Course }: { Course: Course[] }) => {
       </h2>
       <div className="grid gap-6">
         {Course.map((course, idx) => (
-          <CourseCard course={course} key={course._id} idx={idx} />
+          <CourseCard course={course.CourseID} key={course._id} idx={idx} />
         ))}
       </div>
     </div>
