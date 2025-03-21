5765b104215d70cb230b461c0f32f1daa2a5af1d
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2025-03-21 15:41:30 +0500

    Create OrderLoader.tsx

---
+++ components/Order/OrderLoader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderLoader = () => {
+  return (
+    <div className=" min-h-screen flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default OrderLoader
